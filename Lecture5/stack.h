5e5eac3c0142f9ac69007ea12089e28f223b0814
朱少鹏 <zhushaopeng@zhushaopengs-iMac.local>
2018-10-21 22:27:18 +0800

    stack head file

---
+++ Lecture5/stack.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: stack.h
+	> Author: 
+	> Mail: 
+	> Created Time: Sun Oct 21 22:26:04 2018
+ ************************************************************************/
+
+#ifndef _STACK_H
+#define _STACK_H
+
+typedef struct {
+    int *elems;
+    int logicalLen;
+}
+#endif
